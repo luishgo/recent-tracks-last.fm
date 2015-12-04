b5974f2d84b8e2521196c5b96189ea7dce2908e9
Luis HGO <luishgo@gmail.com>
2015-12-04 17:24:02 -0200

    Colorindo o ícone para indicar 'Scrobbling Now'

--- background.js
+++ background.js
@@ -1,114 +1,17 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-var animationFrames = 36;
-var animationSpeed = 10; // ms
 var canvas = document.getElementById('canvas');
 var loggedInImage = document.getElementById('logged_in');
 var canvasContext = canvas.getContext('2d');
-var pollIntervalMin = 1;  // 1 minute
-var pollIntervalMax = 60;  // 1 hour
 var requestTimeout = 1000 * 2;  // 2 seconds
-var rotation = 0;
-var loadingAnimation = new LoadingAnimation();
-
-function getFeedUrl() {
-  var user = 'luishgo';
-  return 'http://ws.audioscrobbler.com/2.0/?api_key=37bdac3246aca8cdde93dfabad064452&method=user.getRecentTracks&user='+user+'&limit=5&format=json';
-}
-
-// A "loading" animation displayed while we wait for the first response from
-// Gmail. This animates the badge text with a dot that cycles from left to
-// right.
-function LoadingAnimation() {
-  this.timerId_ = 0;
-  this.maxCount_ = 8;  // Total number of states in animation
-  this.current_ = 0;  // Current state
-  this.maxDot_ = 4;  // Max number of dots in animation
-}
-
-LoadingAnimation.prototype.paintFrame = function() {
-  var text = "";
-  for (var i = 0; i < this.maxDot_; i++) {
-    text += (i == this.current_) ? "." : " ";
-  }
-  if (this.current_ >= this.maxDot_)
-    text += "";
-
-  chrome.browserAction.setBadgeText({text:text});
-  this.current_++;
-  if (this.current_ == this.maxCount_)
-    this.current_ = 0;
-}
-
-LoadingAnimation.prototype.start = function() {
-  if (this.timerId_)
-    return;
-
-  var self = this;
-  this.timerId_ = window.setInterval(function() {
-    self.paintFrame();
-  }, 100);
-}
-
-LoadingAnimation.prototype.stop = function() {
-  if (!this.timerId_)
-    return;
-
-  window.clearInterval(this.timerId_);
-  this.timerId_ = 0;
-}
-
-function updateIcon() {
-  if (!localStorage.hasOwnProperty('displayTrack')) {
-    chrome.browserAction.setIcon({path:"last.fm.off.png"});
-    chrome.browserAction.setBadgeBackgroundColor({color:[190, 190, 190, 230]});
-    chrome.browserAction.setBadgeText({text:"?"});
-    chrome.browserAction.setTitle({title: "?"});
-  } else {
-    chrome.browserAction.setIcon({path: "last.fm.on.png"});
-    if (localStorage.displayTrack != " - ") {
-      loadingAnimation.start();
-    } else {
-      loadingAnimation.stop();
-      chrome.browserAction.setBadgeText({text:""});
-    }
-    // chrome.browserAction.setBadgeBackgroundColor({color:[208, 0, 24, 255]});
-    // chrome.browserAction.setBadgeText({
-    //   text: localStorage.displayTrack != " - " ? "1" : "0"
-    // });
-    chrome.browserAction.setTitle({title: localStorage.displayTrack != " - " ? localStorage.displayTrack : ""});
-  }
-}
-	
-function scheduleRequest() {
-  console.log('scheduleRequest');
-  var randomness = Math.random() * 2;
-  var exponent = Math.pow(2, localStorage.requestFailureCount || 0);
-  var multiplier = Math.max(randomness * exponent, 1);
-  var delay = Math.min(multiplier * pollIntervalMin, pollIntervalMax);
-  delay = Math.round(delay);
-  console.log('Scheduling for: ' + delay);
-  console.log('Creating alarm');
-  // Use a repeating alarm so that it fires again if there was a problem
-  // setting the next alarm.
-  chrome.alarms.create('refresh', {periodInMinutes: delay});
-}
 
 // ajax stuff
 function startRequest(params) {
-  // Schedule request immediately. We want to be sure to reschedule, even in the
-  // case where the extension process shuts down while this request is
-  // outstanding.
-  if (params && params.scheduleRequest) scheduleRequest();
-
   getRecentTracks(
-    function(artist, track) {
-      updateRecentTrack(artist, track);
+    function(track) {
+      localStorage.track = JSON.stringify(track);
+      updateIcon();
     },
     function() {
-      delete localStorage.displayTrack;
+      delete localStorage.track;
       updateIcon();
     }
   );
@@ -120,16 +23,13 @@ function getRecentTracks(onSuccess, onError) {
     xhr.abort();  // synchronously calls onreadystatechange
   }, requestTimeout);
 
-  function handleSuccess(artist, track) {
-    localStorage.requestFailureCount = 0;
+  function handleSuccess(track) {
     window.clearTimeout(abortTimerId);
-    if (onSuccess)
-      onSuccess(artist, track);
+    if (onSuccess) onSuccess(track);
   }
 
   var invokedErrorCallback = false;
-  function handleError() {
-    ++localStorage.requestFailureCount;
+  function handleError() {  
     window.clearTimeout(abortTimerId);
     if (onError && !invokedErrorCallback)
       onError();
@@ -138,16 +38,15 @@ function getRecentTracks(onSuccess, onError) {
 
   try {
     xhr.onreadystatechange = function() {
-      if (xhr.readyState != 4)
-        return;
+      if (xhr.readyState != 4) return;
 
       if (xhr.response) {
         var firstResult = xhr.response.recenttracks.track[0];
-        if (firstResult['@attr'] && firstResult['@attr'].nowplaying == 'true') {
-          handleSuccess(firstResult.artist['#text'], firstResult.name);
-        } else {
-          handleSuccess("", "");
-        }
+        handleSuccess({
+          artist: firstResult.artist['#text'],
+          name: firstResult.name,
+          playing: firstResult['@attr'] && firstResult['@attr'].nowplaying == 'true'
+        });
         return;
       }
 
@@ -158,93 +57,37 @@ function getRecentTracks(onSuccess, onError) {
       handleError();
     };
 
-    xhr.open("GET", getFeedUrl(), true);
+    xhr.open("GET", getLastFmUrl(), true);
     xhr.responseType = 'json';
     xhr.send(null);
   } catch(e) {
-    console.error(chrome.i18n.getMessage("gmailcheck_exception", e));
+    console.error(chrome.i18n.getMessage("exception", e));
     handleError();
   }
 }
 
-function updateRecentTrack(artist, track) {
-  var displayTrack = artist + ' - ' + track;
-  var changed = localStorage.displayTrack != displayTrack;
-  localStorage.displayTrack = displayTrack;
-  updateIcon();
-  if (changed)
-    animateFlip();
+function getLastFmUrl() {
+  var user = 'luishgo';
+  return 'http://ws.audioscrobbler.com/2.0/?api_key=37bdac3246aca8cdde93dfabad064452&method=user.getRecentTracks&user='+user+'&limit=5&format=json';
 }
 
-function ease(x) {
-  return (1-Math.sin(Math.PI/2+x*Math.PI))/2;
-}
+function updateIcon() {
+  var track = localStorage.hasOwnProperty('track') ? JSON.parse(localStorage.track) : null;
 
-function animateFlip() {
-  rotation += 1/animationFrames;
-  drawIconAtRotation();
-
-  if (rotation <= 1) {
-    setTimeout(animateFlip, animationSpeed);
+  if (track && track.playing) {
+    chrome.browserAction.setIcon({path: "last.fm.on.png"});
+    chrome.browserAction.setTitle({title: track.artist + ' - ' + track.name});
   } else {
-    rotation = 0;
-    updateIcon();
+    chrome.browserAction.setIcon({path:"last.fm.off.png"});
+    chrome.browserAction.setTitle({title: ""});
   }
-}
 
-function drawIconAtRotation() {
-  canvasContext.save();
-  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-  canvasContext.translate(
-      Math.ceil(canvas.width/2),
-      Math.ceil(canvas.height/2));
-  canvasContext.rotate(2*Math.PI*ease(rotation));
-  canvasContext.drawImage(loggedInImage,
-      -Math.ceil(canvas.width/2),
-      -Math.ceil(canvas.height/2));
-  canvasContext.restore();
-
-  chrome.browserAction.setIcon({imageData:canvasContext.getImageData(0, 0,
-      canvas.width,canvas.height)});
-}
-
-function onInit() {
-  console.log('onInit');
-  localStorage.requestFailureCount = 0;  // used for exponential backoff
-  startRequest({scheduleRequest:true, showLoadingAnimation:true});
-  chrome.alarms.create('watchdog', {periodInMinutes:1});
 }
 
 function onAlarm(alarm) {
   console.log('Got alarm', alarm);
-  // |alarm| can be undefined because onAlarm also gets called from
-  // window.setTimeout on old chrome versions.
-  if (alarm && alarm.name == 'watchdog') {
-    onWatchdog();
-  } else {
-    startRequest({scheduleRequest:true, showLoadingAnimation:false});
-  }
+  startRequest({scheduleRequest:true, showLoadingAnimation:false});
 }
 
-function onWatchdog() {
-  chrome.alarms.get('refresh', function(alarm) {
-    if (alarm) {
-      console.log('Refresh alarm exists. Yay.');
-    } else {
-      console.log('Refresh alarm doesn\'t exist!? ' +
-                  'Refreshing now and rescheduling.');
-      startRequest({scheduleRequest:true, showLoadingAnimation:false});
-    }
-  });
-}
-
-chrome.runtime.onInstalled.addListener(onInit);
 chrome.alarms.onAlarm.addListener(onAlarm);
-
-if (chrome.runtime && chrome.runtime.onStartup) {
-  chrome.runtime.onStartup.addListener(function() {
-    console.log('Starting browser... updating icon.');
-    startRequest({scheduleRequest:false, showLoadingAnimation:false});
-    updateIcon();
-  });
-}
+chrome.alarms.create('request', {when: Date.now(), periodInMinutes:1});
